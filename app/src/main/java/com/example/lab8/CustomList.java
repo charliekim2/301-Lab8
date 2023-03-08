4e5e35be6aa78bcbdfa1e533f2603bdfdccf51f2
charliekim2 <charlie2@ualberta.ca>
2023-03-07 17:37:02 -0700

    countCities now returns

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -59,6 +59,8 @@ public class CustomList extends ArrayAdapter<City> {
 
     public void deleteCity(City city){}
 
-    public int countCities(){}
+    public int countCities(){
+        return 0;
+    }
 
 }

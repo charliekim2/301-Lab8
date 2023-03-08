988a5ffe843ca7c8df0dd83cd6b557fccfa7d5cc
charliekim2 <charlie2@ualberta.ca>
2023-03-07 17:23:13 -0700

    added test

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -12,5 +12,23 @@ import java.util.ArrayList;
 
 public class CustomListTest {
 
+    City city;
+    CustomList cityList;
+
+    public CustomListTest(){
+        city = new City("Halifax", "NS");
+        cityList = new CustomList(null, new ArrayList<>());
+    }
+
+    public CustomList mockCityList(){
+        CustomList list = new CustomList(null, new ArrayList<>());
+        return list;
+    }
+    @Test
+    public void testAddCity(){
+        CustomList list = mockCityList();
+        list.addCity(this.city);
+        assertEquals(list.getCount(), 1);
+    }
 
 }
